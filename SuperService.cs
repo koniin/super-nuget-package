75c7813d8e816daf6ce9067e0c3519cb34cb31d8
Henrik Aronsson Länsman <arnssn@gmail.com>
2021-10-09 10:15:52 +0200

    init dotnet

---
+++ SuperService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace super_nuget_package
+{
+    public class SuperService
+    {
+        public string GetTheMessage() {
+            return "Super Message!";
+        }
+    }
+}
